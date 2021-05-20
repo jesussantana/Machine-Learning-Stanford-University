17153128af38c96f0f57b3e8915ffab2f9308445
jesussantana <jesus@esmarketingdigital.com>
2021-05-20 19:22:57 +0200

    Project Organization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This course provides a broad introduction to machine learning, datamining, and statistical pattern recognition',
+    author='Jesus Santana',
+    license='MIT',
+)
