29a2959a893dc9ff41cae4fa6d727a228d316403
jesussantana <jesus@esmarketingdigital.com>
2021-06-02 17:33:54 +0200

    plotData

--- notebooks/Ex1-Linear-Regression/ex1/plotData.m
+++ notebooks/Ex1-Linear-Regression/ex1/plotData.m
@@ -1,4 +1,5 @@
 function plotData(x, y)
+
 %PLOTDATA Plots the data points x and y into a new figure 
 %   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
 %   population and profit.
@@ -16,9 +17,9 @@ figure; % open a new figure window
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
-
-
-
+plot(x, y, 'rx', 'MarkerSize', 10);
+ylabel('Profit in $10,000s');
+xlabel('Population of City in 10,000s')
 
 % ============================================================
 
