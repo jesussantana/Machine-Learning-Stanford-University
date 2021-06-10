5772821ea98d45077572008aa74ad067b2cfb9ab
jesussantana <jesus@esmarketingdigital.com>
2021-06-10 14:58:04 +0200

    Ex 2 Logistic Regression Python

---
+++ notebooks/Ex2-Logistic-Regression/ex2-py/ex2.py
@@ -0,0 +1,71 @@
+import numpy as np
+from scipy.optimize import fmin_bfgs
+
+from sigmoid import sigmoid
+from plotData import plotData
+from costFunction import costFunction
+from plotDecisionBoundary import plotDecisionBoundary
+from predict import predict
+
+
+data = np.loadtxt('ex2data1.txt', delimiter=',')
+X = data[:, [0, 1]]
+y = data[:, [2]]
+
+
+# ==================== Part 1: Plotting ====================
+# We start the exercise by first plotting the data to understand the
+# the problem we are working with.
+
+print('Plotting data with + indicating (y = 1) examples,',
+      'and o indicating (y = 0) examples.\n')
+plotData(X, y, xlabel='Exam 1 score', ylabel='Exam 2 score',
+         legends=['Admitted', 'Not Admitted'])
+
+
+# ============ Part 2: Compute Cost and Gradient ============
+# In this part of the exercise, you will implement the cost and gradient
+# for logistic regression. You neeed to complete the code in
+# costFunction.py
+
+m, n = X.shape
+X = np.hstack((np.ones((m, 1)), X))
+initial_theta = np.zeros(n + 1)
+
+cost, grad = costFunction(initial_theta, X, y)
+print('Cost at initial theta (zeros):', cost)
+print('Gradient at initial theta (zeros):', grad, '\n')
+
+
+# =========== Part 3: Optimizing using fmin_bfgs  ===========
+# In this exercise, you will use a built-in function (fminunc) to find the
+# optimal parameters theta.
+
+cost_function = lambda p: costFunction(p, X, y)[0]
+grad_function = lambda p: costFunction(p, X, y)[1]
+
+theta = fmin_bfgs(cost_function, initial_theta, fprime=grad_function)
+print('theta:', theta, '\n')
+
+plotDecisionBoundary(theta, X[:, 1:], y, xlabel='Exam 1 score', ylabel='Exam 2 score',
+                     legends=['Admitted', 'Not Admitted', 'Decision Boundary'])
+
+
+# ============== Part 4: Predict and Accuracies ==============
+# After learning the parameters, you'll like to use it to predict the outcomes
+# on unseen data. In this part, you will use the logistic regression model
+# to predict the probability that a student with score 45 on exam 1 and
+# score 85 on exam 2 will be admitted.
+#
+# Furthermore, you will compute the training and test set accuracies of
+# our model.
+#
+# Your task is to complete the code in predict.py
+
+prob = sigmoid(np.array([1, 45, 85]).dot(theta))
+print('For a student with scores 45 and 85, we predict an admission',
+      'probability of %f' % prob)
+
+p = predict(theta, X)
+p = np.mean(p == y) * 100
+print('Train Accuracy: %.2f %%' % p)
---
+++ notebooks/Ex2-Logistic-Regression/ex2-py/ex2_reg.py
@@ -0,0 +1,69 @@
+import numpy as np
+from scipy.optimize import fmin_bfgs
+
+from mapFeature import mapFeature
+from plotData import plotData
+from costFunctionReg import costFunctionReg
+from plotDecisionBoundary import plotDecisionBoundary
+from predict import predict
+
+
+# Plot Data
+
+data = np.loadtxt('ex2data2.txt', delimiter=',')
+X = data[:, [0, 1]]
+y = data[:, [2]]
+plotData(X, y, xlabel='Microchip Test 1', ylabel='Microchip Test 2',
+         legends=['y = 1', 'y = 0'])
+
+
+# =========== Part 1: Regularized Logistic Regression ============
+# In this part, you are given a dataset with data points that are not
+# linearly separable. However, you would still like to use logistic
+# regression to classify the data points.
+#
+# To do so, you introduce more features to use -- in particular, you add
+# polynomial features to our data matrix (similar to polynomial
+# regression).
+
+# Note that mapFeature also adds a column of ones for us, so the intercept
+# term is handled
+
+X = mapFeature(X[:, [0]], X[:, [1]])
+
+# Initialize fitting parameters
+
+initial_theta = np.zeros(X.shape[1])
+
+# Set regularization parameter lambda to 1
+
+lamda = 1
+
+cost, _ = costFunctionReg(initial_theta, X, y, lamda)
+print('Cost at initial theta (zeros):', cost)
+
+
+# ============= Part 2: Regularization and Accuracies =============
+# Optional Exercise:
+# In this part, you will get to try different values of lambda and
+# see how regularization affects the decision coundart
+#
+# Try the following values of lambda (0, 1, 10, 100).
+#
+# How does the decision boundary change when you vary lambda? How does
+# the training set accuracy vary?
+
+cost_function = lambda p: costFunctionReg(p, X, y, lamda)[0]
+grad_function = lambda p: costFunctionReg(p, X, y, lamda)[1]
+
+theta = fmin_bfgs(cost_function, initial_theta, fprime=grad_function)
+
+plotDecisionBoundary(theta, X[:, 1:], y, xlabel='Microchip Test 1', ylabel='Microchip Test 2',
+                     legends=['y = 1', 'y = 0', 'Decision Boundary'])
+
+
+# Compute accuracy on our training set
+
+p = predict(theta, X)
+p = np.mean(p == y) * 100
+print('Train Accuracy: %.2f %%' % p)
